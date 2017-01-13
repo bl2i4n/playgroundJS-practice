aa382561990c32f2bd53e4a5f239d9e65f14f075
bl2i4n <bl2i4n@yahoo.com>
2017-01-12 16:46:05 -0800

    cannot get renderer to show blue screen with white square

--- script/main.js
+++ script/main.js
@@ -1,19 +1,8 @@
 var app = playground([
-    create: function(){
-        this.loadImage("ship");
     
-    },
-    
-    ready: function(){
-        /*
-            ready eveny listener - if you want to do something
-            when the loader has finished the job
-        */
-    
-    },
-    
-    render: function(){
-        this.layer.clear("#00088");
-        this.layer.drawImage(this.images.ship,0,0)
-    }   
+   render: function() {
+        this.layer.clear("#000088");
+        //this.layer.fillStyle("#ffffff");
+        //this.layer.fillRect(32, 32, 64, 64);
+    }
 ]);
